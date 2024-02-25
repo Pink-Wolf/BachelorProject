95ecba451f22dcb15dc6294ae8611c9accbab8f8
Pink-Wolf <ponypencilpaper@gmail.com>
2024-02-25 21:22:56 +0100

    Created test vertex- and fragment-shader.
    Graphics.Core.TestApp:
    - created resources vertex.vert and pixel.frag.
    - created compile.bat which compiles the shaders.

---
+++ CompWolf/CompWolf.Graphics.Core.TestApp/compile.bat
@@ -0,0 +1,2 @@
+"%~dp0..\..\extern\CompWolf.Graphics.Core\vulkan\Tools\glslc.exe" "%~dp0vertex.vert" -o "%~dp0vertex.spv"
+"%~dp0..\..\extern\CompWolf.Graphics.Core\vulkan\Tools\glslc.exe" "%~dp0pixel.frag" -o "%~dp0frag.spv"
