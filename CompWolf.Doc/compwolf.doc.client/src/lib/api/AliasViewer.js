b1b787bba837753cde6bc1bd3f9bd6679769014b
Pink-Wolf <ponypencilpaper@gmail.com>
2024-04-24 09:10:15 +0200

    Wrote docs for gpu_buffers. Fixed AliasViewer not using BaseEntityViewer.

--- CompWolf.Doc/compwolf.doc.client/src/lib/api/AliasViewer.js
+++ CompWolf.Doc/compwolf.doc.client/src/lib/api/AliasViewer.js
@@ -1,15 +1,22 @@
+import { Fragment } from "react";
 import { Declaration } from "../CodeComponents";
 import FormattedText from "../FormattedText";
+import BaseEntityViewer from "./BaseEntityViewer";
 
 export default function AliasViewer({ data }) {
+
 	return (
-		<section>
-			<h1 id="Declaration">
-				<Declaration>{data.declaration}</Declaration>
-			</h1>
-			<p id="Description">
-				<FormattedText>{data.detailedDescription}</FormattedText>
-			</p>
-		</section>
+		<BaseEntityViewer data={data} top={
+			<Fragment>
+				<big id="Declaration">
+					<Declaration>{data.declaration}</Declaration>
+				</big>
+				<p id="Description">
+					<FormattedText>{data.detailedDescription}</FormattedText>
+				</p>
+			</Fragment>
+		}>
+
+		</BaseEntityViewer>
 	)
 }
