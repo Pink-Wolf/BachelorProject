c24d1e3a6a7e71e3e926f3ead490f00e87d7a37a
Pink-Wolf <ponypencilpaper@gmail.com>
2024-05-03 16:56:34 +0200

    Simplified declaration in vulkan_types.

--- CompWolf/CompWolf.Graphics.Core/include/private/other/vulkan_types.hpp
+++ CompWolf/CompWolf.Graphics.Core/include/private/other/vulkan_types.hpp
@@ -22,38 +22,38 @@ namespace CompWolf::Private
 		constexpr inline explicit operator bool() const { return value != 0; }
 	};
 
-	struct vulkan_fence_t {};							using vulkan_fence = vulkan_handle64<vulkan_fence_t>;
-	struct vulkan_semaphore_t {};						using vulkan_semaphore = vulkan_handle64<vulkan_semaphore_t>;
+	using vulkan_fence = vulkan_handle64<struct vulkan_fence_t>;
+	using vulkan_semaphore = vulkan_handle64<struct vulkan_semaphore_t>;
 
-	struct vulkan_instance_t {};						using vulkan_instance = vulkan_instance_t*;
-	struct vulkan_debug_messenger_t {};					using vulkan_debug_messenger = vulkan_debug_messenger_t*;
-	struct vulkan_physical_device_t {};					using vulkan_physical_device = vulkan_physical_device_t*;
-	struct vulkan_device_t {};							using vulkan_device = vulkan_device_t*;
-	struct vulkan_queue_t {};							using vulkan_queue = vulkan_queue_t*;
+	using vulkan_instance = struct vulkan_instance_t*;
+	using vulkan_debug_messenger = struct vulkan_debug_messenger_t*;
+	using vulkan_physical_device = struct vulkan_physical_device_t*;
+	using vulkan_device = struct vulkan_device_t*;
+	using vulkan_queue = struct vulkan_queue_t*;
 
-	struct glfw_window_t {};							using glfw_window = glfw_window_t*;
-	struct vulkan_surface_t {};							using vulkan_surface = vulkan_handle64<vulkan_surface_t>;
-	struct vulkan_swapchain_t {};						using vulkan_swapchain = vulkan_handle64<vulkan_swapchain_t>;
-	struct surface_format_handle_t {};					using surface_format_handle = surface_format_handle_t*;
+	using glfw_window = struct glfw_window_t*;
+	using vulkan_surface = vulkan_handle64<struct vulkan_surface_t>;
+	using vulkan_swapchain = vulkan_handle64<struct vulkan_swapchain_t>;
+	using surface_format_handle = struct surface_format_handle_t*;
 
-	struct vulkan_image_view_t {};						using vulkan_image_view = vulkan_handle64<vulkan_image_view_t>;
-	struct vulkan_sampler_t {};							using vulkan_sampler = vulkan_handle64<vulkan_sampler_t>;
+	using vulkan_image_view = vulkan_handle64<struct vulkan_image_view_t>;
+	using vulkan_sampler = vulkan_handle64<struct vulkan_sampler_t>;
 
-	struct shader_field_info_handle_t {};				using shader_field_info_handle = shader_field_info_handle_t*;
-	struct vulkan_buffer_t {};							using vulkan_buffer = vulkan_handle64<vulkan_buffer_t>;
-	struct vulkan_memory_t {};							using vulkan_memory = vulkan_handle64<vulkan_memory_t>;
-	struct vulkan_shader_t {};							using vulkan_shader = vulkan_handle64<vulkan_shader_t>;
-	struct vulkan_pipeline_layout_t {};					using vulkan_pipeline_layout = vulkan_handle64<vulkan_pipeline_layout_t>;
-	struct vulkan_pipeline_layout_descriptor_t {};		using vulkan_pipeline_layout_descriptor = vulkan_handle64<vulkan_pipeline_layout_descriptor_t>;
-	struct vulkan_descriptor_pool_t {};					using vulkan_descriptor_pool = vulkan_handle64<vulkan_descriptor_pool_t>;
-	struct vulkan_descriptor_set_t {};					using vulkan_descriptor_set = vulkan_handle64<vulkan_descriptor_set_t>;
-	struct vulkan_render_pass_t {};						using vulkan_render_pass = vulkan_handle64<vulkan_render_pass_t>;
-	struct vulkan_pipeline_t {};						using vulkan_pipeline = vulkan_handle64<vulkan_pipeline_t>;
-	struct vulkan_frame_buffer_t {};					using vulkan_frame_buffer = vulkan_handle64<vulkan_frame_buffer_t>;
-	struct vulkan_command_pool_t {};					using vulkan_command_pool = vulkan_handle64<vulkan_command_pool_t>;
+	using shader_field_info_handle = struct shader_field_info_handle_t*;
+	using vulkan_buffer = vulkan_handle64<struct vulkan_buffer_t>;
+	using vulkan_memory = vulkan_handle64<struct vulkan_memory_t>;
+	using vulkan_shader = vulkan_handle64<struct vulkan_shader_t>;
+	using vulkan_pipeline_layout = vulkan_handle64<struct vulkan_pipeline_layout_t>;
+	using vulkan_pipeline_layout_descriptor = vulkan_handle64<struct vulkan_pipeline_layout_descriptor_t>;
+	using vulkan_descriptor_pool = vulkan_handle64<struct vulkan_descriptor_pool_t>;
+	using vulkan_descriptor_set = vulkan_handle64<struct vulkan_descriptor_set_t>;
+	using vulkan_render_pass = vulkan_handle64<struct vulkan_render_pass_t>;
+	using vulkan_pipeline = vulkan_handle64<struct vulkan_pipeline_t>;
+	using vulkan_frame_buffer = vulkan_handle64<struct vulkan_frame_buffer_t>;
+	using vulkan_command_pool = vulkan_handle64<struct vulkan_command_pool_t>;
 	struct vulkan_command_t {};							using vulkan_command = vulkan_command_t*;
 
-	struct vulkan_image_t {};							using vulkan_image = vulkan_handle64<vulkan_image_t>;
+	using vulkan_image = vulkan_handle64<struct vulkan_image_t>;
 }
 
 #endif // ! COMPWOLF_GRAPHICS_PRIVATE_VULKAN_TYPES_HEADER
