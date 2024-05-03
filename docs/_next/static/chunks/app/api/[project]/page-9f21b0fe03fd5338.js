b0c7e7bb037bb7821f87cf4ef479575a67944294
Pink-Wolf <ponypencilpaper@gmail.com>
2024-05-03 09:54:56 +0200

    Updated docs-folder with new version of docs.

---
+++ docs/_next/static/chunks/app/api/[project]/page-9f21b0fe03fd5338.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[892,160,931,105,558,722,163],{515:function(n,e,u){Promise.resolve().then(u.t.bind(u,5250,23))}},function(n){n.O(0,[250,971,69,744],function(){return n(n.s=515)}),_N_E=n.O()}]);
--- docs/_next/static/chunks/app/layout-8f2a00cf532282be.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{1014:function(n,e,t){Promise.resolve().then(t.t.bind(t,5250,23)),Promise.resolve().then(t.t.bind(t,1359,23))},1359:function(){}},function(n){n.O(0,[250,971,69,744],function(){return n(n.s=1014)}),_N_E=n.O()}]);
