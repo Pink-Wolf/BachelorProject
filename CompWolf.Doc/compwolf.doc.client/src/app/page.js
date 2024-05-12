5f7f5f517f578973bd81377970642648062722c5
Pink-Wolf <ponypencilpaper@gmail.com>
2024-05-12 17:45:37 +0200

    Fixed unescaped ' in docs.

--- CompWolf.Doc/compwolf.doc.client/src/app/page.js
+++ CompWolf.Doc/compwolf.doc.client/src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
             <h1>Welcome to the CompWolf website.</h1>
             <p>To learn how to use the CompWolf libraries, consider going through <Link href="/tutorial">these tutorials</Link>.</p>
             <p>If you wish to find specific information about CompWolf, consider checking out <Link href="/api/">its api documentation.</Link>.</p>
-            <p>If you wish to see CompWolf's source code, check out its <Link href="https://github.com/Pink-Wolf/BachelorProject">GitHub repository</Link>.</p>
+            <p>If you wish to see CompWolf&apos;s source code, check out its <Link href="https://github.com/Pink-Wolf/BachelorProject">GitHub repository</Link>.</p>
         </div>
     );
 }
